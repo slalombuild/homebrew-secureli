9b1c19811e8b95045cf7c3343e6da7d1d5557c71
Raul Centeno <raul.centeno@slalom.com>
2023-04-10 19:14:14 -0600

    Add new override

--- Formula/lib/custom_download_strategy.rb
+++ Formula/lib/custom_download_strategy.rb
@@ -105,4 +105,8 @@ class GitHubPrivateRepositoryReleaseDownloadStrategy < GitHubPrivateRepositoryDo
     release_url = "https://api.github.com/repos/#{@owner}/#{@repo}/releases/tags/#{@tag}"
     GitHub::API.open_rest(release_url)
   end
+
+  def resolve_url_basename_time_file_size(url, timeout: nil)
+    [download_url, "", Time.now, 0, false]
+  end
 end
