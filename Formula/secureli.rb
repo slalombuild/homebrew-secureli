3c90f59eeb4cf0eadacaedbf20315563606a4493
Raul Centeno <raul.centeno@slalom.com>
2023-04-14 09:36:15 -0600

    Remove custom download strategy usage

--- Formula/secureli.rb
+++ Formula/secureli.rb
@@ -2,7 +2,7 @@ class Secureli < Formula
     include Language::Python::Virtualenv
     desc "SeCureLI is a tool that enables you to experience the delight of building products by helping you get ideas from your head into working software as frictionlessly as possible, in a reliable, secure, scalable, and observable way"
     homepage "https://github.com/slalombuild/secureli"
-    url "https://github.com/slalombuild/secureli/releases/download/v0.2.4/secureli-0.2.4.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    url "https://github.com/slalombuild/secureli/releases/download/v0.2.4/secureli-0.2.4.tar.gz"
     version "0.2.4"
     sha256 "7b12946d767037e6bc3762644895c5d2c3f252e686a2d9c815c6e6209fdc425f"
     license "Apache-2.0"
